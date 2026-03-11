e01538c668552c20b1ef60889578a756212e5f7e
alexmutsinzi2021-collab <alexmutsinzi2021@gmail.com>
2026-03-11 19:56:24 +0200

    Update models.py
    created models for student

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class students(models.Model):
+    name= models.CharField(max_length=100)
+    reg_number=models.CharField(max_length=50)
+    email=models.EmailField()
+    course=models.CharField(max_length=100)
+    year_of_study=models.IntegerField()
+    phone_number=models.CharField(max_length=10)
+
 # Create your models here.
