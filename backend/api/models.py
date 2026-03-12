5b31fa1272845a2f63dd983ed9bac60781377597
alexmutsinzi2021-collab <alexmutsinzi2021@gmail.com>
2026-03-12 13:05:32 +0200

    added the administrator model, debugged errors and ran migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,20 +8,27 @@ class students(models.Model):
     year_of_study=models.IntegerField()
     phone_number=models.CharField(max_length=10)
 
-    class academic_supervisor(models.Model):
-     name=models.Charfeld(max_length=100)
-     user_id=models.Charfield(max_length=50)
+class academic_supervisor(models.Model):
+     name=models.CharField(max_length=100)
+     user_id=models.CharField(max_length=50)
      email=models.EmailField()
-     phone=models.CharFieldField(max_length=13)
-     department=models.Charfield(max_length=50)
+     phone=models.CharField(max_length=13)
+     department=models.CharField(max_length=50)
      office_location=models.CharField(max_length=100)
      specialization= models.CharField()
 
-     class workplace_supervisor(models.Model):
+class workplace_supervisor(models.Model):
       name=models.CharField(max_length=100)
       email=models.EmailField() 
       phone=models.CharField(max_length=14)
       position=models.CharField(max_length=100)
+
+class administrator(models.Model):
+     name=models.CharField(max_length=100)
+     email=models.CharField()
+     phone=models.CharField(max_length=15)
+     role=models.CharField(200)
+
      
      
     
