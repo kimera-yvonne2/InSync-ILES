81534f5470e1c2a05db9454a42c656d0b5d8f057
robinsonmugabi <robinsonmugabi@gmail.com>
2026-03-24 11:17:56 +0300

    am back

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class students(models.Model):
+class Students(models.Model):
     id=models.AutoField(primary_key=True)
     name= models.CharField(max_length=100)
     reg_number=models.CharField(max_length=50)
