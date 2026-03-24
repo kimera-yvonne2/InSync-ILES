1fa7a9b0404ac0d88c5413bb95ccf2035d0fc712
kimera-yvonne2 <yvonnekimera2@gmail.com>
2026-03-24 21:53:40 +0300

    Update frontend index and backend serializers

---
+++ backend/api/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from .models import (
+    students,
+    academic_supervisor,
+    workplace_supervisor,
+    administrator,
+    company,
+    internship,
+    appplication_model,
+    internshipreport_model,
+    WeeklyLog
+)
+
+class StudentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = students
+        fields = '__all__' 
+
+class AcademicSupervisorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = academic_supervisor
+        fields = '__all__'
+
+class WorkplaceSupervisorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = workplace_supervisor
+        fields = '__all__'
+
+class AdministratorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = administrator
+        fields = '__all__'
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = company
+        fields = '__all__'
+
+class InternshipSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = internship
+        fields = '__all__'
+
+class ApplicationModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = appplication_model
+        fields = '__all__'
+
+class InternshipReportModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = internshipreport_model
+        fields = '__all__'
+
+class WeeklyLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WeeklyLog
+        fields = '__all__'
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    'api',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -116,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", # Standard for Create-React-App
+    "http://localhost:5173", # Standard for Vite/React          
+]
