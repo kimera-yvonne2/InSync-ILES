407650773d10da3f632cb9d4f26f06d035f8fe71
alexmutsinzi2021-collab <alexmutsinzi2021@gmail.com>
2026-04-01 15:11:40 +0200

    have undone the deletion of models

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
-14)
+class Students(models.Model): 
+    
+    id=models.AutoField(primary_key=True)
+    name= models.CharField(max_length=100)
+    reg_number=models.CharField(max_length=50)
+    email=models.EmailField()
+    course=models.CharField(max_length=100)
+    year_of_study=models.IntegerField()
+    phone_number=models.CharField(max_length=10)
+    password=models.CharField(max_length=14)
     cv=models.FileField(upload_to="cvs")
     date_craeted=models.DateTimeField(auto_now_add=True)
 
