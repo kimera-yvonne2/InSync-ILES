ad3179ddcf568c6f81d2ca2c0c901606d6d72b42
alexmutsinzi2021-collab <alexmutsinzi2021@gmail.com>
2026-04-01 15:15:47 +0200

    Have brought back the manage.py file and the backend project folder

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import students
+#from .models import students
 from .serializers import studentsSerializer
 
 class studentListCreate(generics.ListCreateAPIView):
