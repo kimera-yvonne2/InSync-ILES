61ff5f3b74ff0176c16d24ad9e448ffa3c030ba1
robinsonmugabi <robinsonmugabi@gmail.com>
2026-04-08 09:04:12 +0300

    successful migrations

--- backend/placements/models.py
+++ backend/placements/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class InternshipPlacement(models.Model):
-    student = models.OneToOneField(settings.AUTH_USER_MODEL, on_visit=models.CASCADE, related_name='placement')
+    student = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='placement')
     workplace_supervisor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='supervised_interns')
     company_name = models.CharField(max_length=255)
     start_date = models.DateField()
