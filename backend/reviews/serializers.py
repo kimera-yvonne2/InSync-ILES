332a738d979f6893cba652d8840dd32cf65e057c
robinsonmugabi <robinsonmugabi@gmail.com>
2026-04-09 16:08:18 +0300

    Review serializers

---
+++ backend/reviews/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import SupervisorReview
+
+class SupervisorReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SupervisorReview
+        fields = ['id', 'weekly_log', 'supervisor', 'comments', 'technical_score', 'professionalism_score', 'date_reviewed']
+        read_only_fields = ['supervisor', 'date_reviewed']
