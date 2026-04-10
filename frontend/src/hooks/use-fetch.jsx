62035976c2746471428707db71d7a64a7b8cc72a
kimera-yvonne2 <yvonnekimera2@gmail.com>
2026-04-10 12:05:58 +0300

    added another hook

---
+++ frontend/src/hooks/use-fetch.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError(err.message ||'An error occurred');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);  //It re-runs if the URL changes
+
+  return { data, loading, error };
+};
+
+export default useFetch;
