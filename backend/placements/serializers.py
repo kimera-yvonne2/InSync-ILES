5b9fd97baedcd6041c2bcf652b9cf65c78be19ab
alexmutsinzi2021-collab <alexmutsinzi2021@gmail.com>
2026-04-11 16:11:17 +0300

    modified serializers

--- backend/placements/serializers.py
+++ backend/placements/serializers.py
@@ -7,7 +7,6 @@ class InternshipPlacementSerializer(serializers.ModelSerializer):
     academic_supervisor = serializers.StringRelatedField(read_only=True)
     workplace_supervisor = serializers.StringRelatedField(read_only=True)
     
-
     class Meta:
         model = InternshipPlacement
         fields = '__all__'
