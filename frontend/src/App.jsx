4c806b80f1dfde53ea5bfbf25996005094b34f93
Walid26619 <kahumawalid@yahoo.com>
2026-04-15 09:47:15 +0300

    Added form submissions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Features from "./Features";
+import LogForm from "./LogForm";
 import Footer from "./Footer";
+
 function App() {
   return (
     <>
     <Navbar/>
     <Hero/>
     <Features/>
+    <LogForm/>
     <Footer/>
     </>
   );
