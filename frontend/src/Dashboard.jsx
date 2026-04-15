705fdb896168d9d119a0cb4d3a402ba5ed9975b6
Walid26619 <kahumawalid@yahoo.com>
2026-04-15 11:49:47 +0300

    Cleaned Dashboard

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,22 +1,29 @@
 import LogForm from "./LogForm";
+import LogList from "./LogList";
+import { useState } from "react";
 
 function Dashboard({ goHome }) {
+  const [logs, setLogs] = useState([]);
+
   return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
-      <button
-        onClick={goHome}
-        style={{
-          marginBottom: "20px",
-          padding: "10px 20px",
-          cursor: "pointer"
-        }}
-      >
-        Back to Home
-      </button>
+    <div style={{ padding: "20px" }}>
+      <button onClick={goHome}>Back to Home</button>
 
       <h1>Intern Dashboard</h1>
 
-      <LogForm />
+      <div style={{ display: "flex", gap: "20px", marginTop: "20px" }}>
+        
+        {/* LEFT: FORM */}
+        <div style={{ flex: 1 }}>
+          <LogForm logs={logs} setLogs={setLogs} />
+        </div>
+
+        {/* RIGHT: LIST */}
+        <div style={{ flex: 1 }}>
+          <LogList logs={logs} />
+        </div>
+
+      </div>
     </div>
   );
 }
