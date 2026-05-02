0d3c47f644d99917d16ed0e804d84dd33409aea7
Walid26619 <kahumawalid@yahoo.com>
2026-05-02 16:58:01 +0300

    Updated models

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -1,48 +1,47 @@
 from django.db import models
-from django.conf import settings 
+from django.conf import settings
+
 
 class WeeklyLog(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DRAFT', 'Draft'
         SUBMITTED = 'SUBMITTED', 'Submitted'
-        REVIEWED ='REVIEWED', 'Reveiwed'
+        REVIEWED = 'REVIEWED', 'Reviewed'
         APPROVED = 'APPROVED', 'Approved'
-        
 
     student = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete =models.CASCADE,
-        related_name ='logs'
-    ) 
-    week_number = models.PositiveIntegerField()
-    start_date = models.DateField() 
-    end_date =models.DateField()
-    
-    activities_performed = models.TextField(blank =True , null = True)
-    lessons_learned = models.TextField(blank =True ,null= True)
-    challenges_faced = models.TextField(blank =True, null =True)
-    Academic_supervisor=models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL, 
-        null=True,
-        related_name="academic_logs"
-
+        on_delete=models.CASCADE,
+        related_name='logs'
     )
-    placement_supervisor=models.ForeignKey(
+    week_number = models.PositiveIntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    activities_performed = models.TextField(blank=True, null=True)
+    lessons_learned = models.TextField(blank=True, null=True)
+    challenges_faced = models.TextField(blank=True, null=True)
+
+    Academic_supervisor = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True,
-        related_name="placement_logs"
-
+        related_name='academic_logs'
+    )
+    placement_supervisor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='placement_logs'
     )
 
     status = models.CharField(
-        max_length =20,
-        choices =Status.choices,
-        default =Status.DRAFT
-    )  
+        max_length=20,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
 
-    created_at = models.DateTimeField(auto_now_add =True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -50,8 +49,5 @@ class WeeklyLog(models.Model):
         
 
     def __str__(self):
-        return f"Week {self.week_number} - {self.student.username}" 
-         
-
-                                    
-# Create your models here.
+        # FIX: was student.username but USERNAME_FIELD is email
+        return f"Week {self.week_number} - {self.student.email}"
